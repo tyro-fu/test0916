01fc8c8741691525423b06465c512a618a7c4820
newLearn0113 <1455904316@qq.com>
2019-09-18 20:06:35 +0800

    更新了仓库的初始字段以及设置方法

--- client/src/store.js
+++ client/src/store.js
@@ -5,10 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+      user:null,
+      products:[],
+      oPro:null
   },
   mutations: {
-
+    //设置用户信息
+      setUesr(state,value){
+      state.user=value
+      },
+      //设置商品展示信息
+      setPro(state,value){
+        state.products=value
+      },
+      //设置商品详情信息
+      setoPro(state,value){
+        state.oPro=value
+      }
   },
   actions: {
 
